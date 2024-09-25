8525ed9ec6440e8269ac3168745f7108591d400a
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-09-25 12:46:22 +0700

    msg: pusing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = buatRoutes(__dirname)
 const init = async () => {
 
     const server = Hapi.server({
-        host: '0.0.0.0',
+        host: 'localhost',
         port: process.env.PORT || 5000
     })
 
